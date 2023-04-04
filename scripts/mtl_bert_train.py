3736f49b8b589bc119f9043b7262bfcad4d813de
bogdan.nicula <bogdan.nicula22@gmail.com>
2023-04-04 11:17:54 +0300

    Apr 4 experiments

--- scripts/mtl_bert_train.py
+++ scripts/mtl_bert_train.py
@@ -135,8 +135,8 @@ def experiment(task_imp_weights=[], bert_model="bert-base-cased", lr=1e-3, num_e
 
 def objective(trial):
     class_weighting = "[2,2,1,5]"#trial.suggest_categorical("class_weighting", ["[1,1,1,1]", "[1,1,1,3]", "[2,2,1,5]"])
-    lr = trial.suggest_float("lr", 5e-4, 2e-3, log=True)
-    lr_warmup = trial.suggest_int("lr_warmup", 6, 9, step=1)
+    lr = trial.suggest_float("lr", 1e-4, 5e-3, log=True)
+    lr_warmup = trial.suggest_int("lr_warmup", 5, 10, step=1)
     # hidden_units = trial.suggest_int("hidden_units", 50, 200, step=50)
     # filtering = trial.suggest_categorical("filtering", ["true", "false"])
     grad_norm = "true" # trial.suggest_categorical("grad_norm", ["true", "false"])
@@ -147,12 +147,12 @@ def objective(trial):
         project="optuna",
         entity="bogdan-nicula22",  # NOTE: this entity depends on your wandb account.
         config=config,
-        group="param-search-v3",
+        group="param-search-v4",
         reinit=True,
     )
     loss = experiment([int(c) for c in class_weighting[1:-1].split(",")], bert_model="roberta-base",
-                      lr=lr, num_epochs=30, use_grad_norm=grad_norm == "true", use_filtering=True, trial=trial,
-                      hidden_units=150, lr_warmup=lr_warmup)
+                      lr=lr, num_epochs=40, use_grad_norm=grad_norm == "true", use_filtering=True, trial=trial,
+                      hidden_units=100, lr_warmup=lr_warmup)
 
 
     # report the final validation accuracy to wandb
@@ -170,7 +170,7 @@ if __name__ == '__main__':
         study_name="param-search-study",
         pruner=optuna.pruners.MedianPruner(),
     )
-    study.optimize(objective, n_trials=15, timeout=None)
+    study.optimize(objective, n_trials=10, timeout=None)
 
     # print("=" * 33)
     # experiment([2, 2, 1, 5], bert_model="roberta-base", lr=2e-4, num_epochs=25, use_grad_norm=True, use_filtering=False)
